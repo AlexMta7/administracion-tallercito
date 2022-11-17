792150ab466821a1cc3a150667a67072a0a93dcd
Alex <alexrobertomata@hotmail.com>
2022-11-17 11:18:56 -0600

    CRUD de Employees completo

--- src/main/java/com/project/taller/controller/rest/EmployeeController.java
+++ src/main/java/com/project/taller/controller/rest/EmployeeController.java
@@ -1,13 +1,51 @@
 package com.project.taller.controller.rest;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.project.taller.model.EmployeeModel;
 import com.project.taller.service.EmployeeService;
 
 @RestController
+@RequestMapping(value = "/api/employees")
 public class EmployeeController {
 
     @Autowired
     private EmployeeService employeeService;
+
+    /*
+     * Create
+     * Read
+     * Update
+     * Delete
+     */
+
+    // * Create
+    @PostMapping()
+    public String addEmployee(@RequestBody EmployeeModel employee) {
+        employeeService.addEmployee(employee);
+        return "OK";
+    }
+
+    // * Read
+    @GetMapping()
+    public List<EmployeeModel> getEmployees() {
+        return employeeService.getEmployees();
+    }
+
+    // * Update
+    @PutMapping()
+    public String updateEmployee(@RequestBody EmployeeModel employee) {
+        employeeService.updateEmployee(employee);
+        return "OK";
+    }
+
+    // * Delete
+    @DeleteMapping("/{id}")
+    public String deleteEmployee(@PathVariable Long id) {
+        employeeService.deleteEmployee(id);
+        return "OK";
+    }
 }
--- src/main/java/com/project/taller/service/EmployeeService.java
+++ src/main/java/com/project/taller/service/EmployeeService.java
@@ -1,8 +1,11 @@
 package com.project.taller.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.project.taller.model.EmployeeModel;
 import com.project.taller.repository.EmployeeRepository;
 
 @Service
@@ -10,4 +13,21 @@ public class EmployeeService {
 
     @Autowired
     private EmployeeRepository employeeRepository;
+
+    public void addEmployee(EmployeeModel employee) {
+        employeeRepository.save(employee);
+    }
+
+    public List<EmployeeModel> getEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public void updateEmployee(EmployeeModel employee) {
+        employeeRepository.save(employee);
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepository.deleteById(id);
+    }
+
 }
